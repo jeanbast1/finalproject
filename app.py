6b1c7b51827b1be3cc9ff588217fee1d73a2bb8c
jbd <jeanbastien.dussart@gmail.com>
2022-01-13 11:40:53 +0100

    bla

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask, Markup, redirect, render_template, request, session
+from flask import Flask, render_template, request
 import requests
-import sqlite3
 from datetime import datetime
+
+import sqlite3
+con = sqlite3.connect('cryptos.db')
+db = con.cursor()
+
+
+# db.execute("CREATE TABLE [IF NOT EXISTS] crypto_data (year integer, month integer, day integer, value real)")
 
 app = Flask(__name__)
 
@@ -15,7 +21,6 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-''' url_for('static', filename='style.css') '''
 
 url = 'https://api.kraken.com/0/public/OHLC?pair='
 
@@ -25,8 +30,7 @@ def get_crypto_data():
         crypto = request.form.get("cryptos")
         resp = requests.get(url + crypto + 'EUR&interval=1440&since=1639782000').json()
         result = resp['result']
-        dates = []
-        prices = []
+
         # iterate through keys of result
         for key in result:
             # if key is crypto-currency pair
@@ -35,20 +39,13 @@ def get_crypto_data():
                 result = result[key]
                 for date in result:
                     stamp = datetime.fromtimestamp(date[0])
-                    stamp = str(datetime.date(stamp))
-                    dates.append(stamp)
-                    prices.append(float(date[1]))
+                    year = datetime.stamp.year()
+                    print(year)
+                    db.execute("INSERT INTO cryptos (year, month, day, value) VALUES(?,?,?,?)", int(stamp.datetime.year), int(stamp.datetime.month), int(stamp.datetime.day), float(date[1]))
+                    # dates.append(stamp)
+                    # prices.append(float(date[1]))
+        crypto_data = db.execute("SELECT * FROM cryptos")
 
-        return render_template('index.html', dates=dates, prices=prices)
+        return render_template('index.html', crypto_data=crypto_data)
     else:
         return render_template("index.html")
-
-
-if __name__ == '__main__':
-    app.run(host="localhost", port=8000, debug=True)
-
-'''
-# Create and connect database
-conn = sqlite3.connect('cs50final_database.db')
-db = conn.cursor()
-'''
