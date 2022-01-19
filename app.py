39489c1ef087f86856a85d6e510ce19cc3542583
jbd <jeanbastien.dussart@gmail.com>
2022-01-19 19:34:55 +0100

    graph is working dynamic colors in graph function to remove cryptos

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import requests
 import time
 import datetime
@@ -17,9 +17,6 @@ def after_request(response):
     return response
 
 
-url = 'https://api.kraken.com/0/public/OHLC?pair='
-
-
 @app.route('/', methods=['POST', 'GET'])
 def get_crypto_data():
 
@@ -32,13 +29,26 @@ def get_crypto_data():
     if request.method == 'POST' and "form1" in request.form:
         crypto_to_add = request.form.get('crypto')
         db.execute("UPDATE crypto_selected SET selected = 1 WHERE symbol = ?;", [crypto_to_add])
+        con.commit()
+        con.close()
+        return redirect("/")
+
+    # change selected crypto status in database
+    if request.method == 'POST' and "form3" in request.form:
+        crypto_to_remove = request.form.get('crypto_to_unselect')
+        db.execute("UPDATE crypto_selected SET selected = 0 WHERE symbol = ?;", [crypto_to_remove])
+        con.commit()
+        con.close()
+        return redirect("/")
 
     # change selected startdate in database
     if request.method == 'POST' and "form2" in request.form:
         date = request.form.get('date')
         date_unix = (int(time.mktime(datetime.datetime.strptime(date, "%Y-%m-%d").timetuple())) + 3600) * 1000
-        print(date_unix)
         db.execute("UPDATE date_selected SET date = ?, unix = ?", (date, date_unix))
+        con.commit()
+        con.close()
+        return redirect("/")
 
     # change selected crypto status in database
     if request.method == 'GET':
@@ -52,6 +62,13 @@ def get_crypto_data():
             date_selected = row
         date_unix = (int(time.mktime(datetime.datetime.strptime(date_selected, "%Y-%m-%d").timetuple())) + 3600) * 1000
 
+        # add the timestamps (dates) for the values
+        timestamps = []
+        qdata = db.execute("SELECT DISTINCT date FROM crypto_data WHERE unix >= ? ORDER BY unix", [date_unix])
+        for timestamp in qdata:
+            timestamps.append(timestamp)
+
+        # add the values and rebase values to 100
         for crypto in selected_cryptos:
             values[crypto] = []
             qdata = db.execute("SELECT value FROM crypto_data WHERE unix >= ? AND symbol = ? ORDER BY unix", (date_unix, crypto))
@@ -62,15 +79,10 @@ def get_crypto_data():
             for y in range(1, len(values[crypto])):
                 values[crypto][y] = 100 * values[crypto][y] / startvalue
 
-        timestamps = []
-        qdata = db.execute("SELECT DISTINCT date FROM crypto_data WHERE unix >= ? ORDER BY unix", [date_unix])
-        for timestamp in qdata:
-            timestamps.append(timestamp)
+        colors = {'BTC': '#F0544F', 'ETH': '#92DCE5', 'ADA': '#291F1E', 'LTC': '#1C6E8C', 'UNI': '#274156', 'LINK': '#FBB13C', 'XRP': '#1B998B', 'SOL': '#09BC8A', 'DOT': '#C2E812'};
+        con.commit()
+        con.close()
 
-        return render_template('index.html', timestamps=timestamps, values=values, date_selected=date_selected, selected_cryptos=selected_cryptos)
-
-    # disconnect database
-    con.commit()
-    con.close()
+        return render_template('index.html', colors=colors, timestamps=timestamps, values=values, date_selected=date_selected, selected_cryptos=selected_cryptos)
 
     return render_template('index.html')
