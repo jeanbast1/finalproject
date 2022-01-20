2dae7663c6bc1336e109102757f4dc8d8e7e55e4
jbd <jeanbastien.dussart@gmail.com>
2022-01-20 18:39:30 +0100

    new database working

--- app.py
+++ app.py
@@ -44,34 +44,35 @@ def get_crypto_data():
     # change selected startdate in database
     if request.method == 'POST' and "form2" in request.form:
         date = request.form.get('date')
-        date_unix = (int(time.mktime(datetime.datetime.strptime(date, "%Y-%m-%d").timetuple())) + 3600) * 1000
-        db.execute("UPDATE date_selected SET date = ?, unix = ?", (date, date_unix))
+        date_unix = int(time.mktime(datetime.datetime.strptime(date, "%Y-%m-%d").timetuple()))
+        db.execute("UPDATE dates SET selected = 0")
+        con.commit()
+        db.execute("UPDATE dates SET selected = 1 WHERE unix = ?", [date_unix])
         con.commit()
         con.close()
         return redirect("/")
 
-    # change selected crypto status in database
+    # prep data for render
     if request.method == 'GET':
         selected_cryptos = []
         values = {}
         qcryptos = db.execute("SELECT symbol FROM crypto_selected WHERE selected = 1")
         for row in qcryptos:
             selected_cryptos.append(row)
-        qdate = db.execute("SELECT date FROM date_selected")
+        qdate = db.execute("SELECT unix FROM dates WHERE selected = 1")
         for row in qdate:
-            date_selected = row
-        date_unix = (int(time.mktime(datetime.datetime.strptime(date_selected, "%Y-%m-%d").timetuple())) + 3600) * 1000
+            date_unix = row
 
         # add the timestamps (dates) for the values
         timestamps = []
-        qdata = db.execute("SELECT DISTINCT date FROM crypto_data WHERE unix >= ? ORDER BY unix", [date_unix])
+        qdata = db.execute("SELECT DISTINCT date FROM dates WHERE unix >= ? ORDER BY unix", [date_unix])
         for timestamp in qdata:
             timestamps.append(timestamp)
 
         # add the values and rebase values to 100
         for crypto in selected_cryptos:
             values[crypto] = []
-            qdata = db.execute("SELECT value FROM crypto_data WHERE unix >= ? AND symbol = ? ORDER BY unix", (date_unix, crypto))
+            qdata = db.execute("SELECT value FROM crypto_data WHERE date_unix >= ? AND symbol = ? ORDER BY date_unix", (date_unix, crypto))
             for x in qdata:
                 values[crypto].append(x)
             startvalue = values[crypto][0]
@@ -79,10 +80,20 @@ def get_crypto_data():
             for y in range(1, len(values[crypto])):
                 values[crypto][y] = 100 * values[crypto][y] / startvalue
 
+        # prepare EUR baseline values
+        EURvalues = []
+        qdataEUR = db.execute("SELECT value FROM crypto_data WHERE date_unix >= ? AND symbol = ? ORDER BY date_unix", (date_unix, "EUR"))
+        for x in qdataEUR:
+            EURvalues.append(x)
+        startvalue = EURvalues[0]
+        EURvalues[0] = 100
+        for y in range(1, len(EURvalues)):
+            EURvalues[y] = 100 * EURvalues[y] / startvalue
+
         colors = {'BTC': '#F0544F', 'ETH': '#92DCE5', 'ADA': '#291F1E', 'LTC': '#1C6E8C', 'UNI': '#274156', 'LINK': '#FBB13C', 'XRP': '#1B998B', 'SOL': '#09BC8A', 'DOT': '#C2E812'};
         con.commit()
         con.close()
 
-        return render_template('index.html', colors=colors, timestamps=timestamps, values=values, date_selected=date_selected, selected_cryptos=selected_cryptos)
+        return render_template('index.html', colors=colors, timestamps=timestamps, EURvalues=EURvalues, values=values, selected_cryptos=selected_cryptos)
 
     return render_template('index.html')
